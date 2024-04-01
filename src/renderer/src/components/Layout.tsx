df8a35bcd0b64fdafda5315438949a86f0531f31
jravalug <jravalug@gmail.com>
2024-04-01 00:57:19 -0400

    remove useless class

--- src/renderer/src/components/Layout.tsx
+++ src/renderer/src/components/Layout.tsx
@@ -11,7 +11,7 @@ export const RootLayout = ({ children, className, ...props }: ComponentProps<'ma
 
 export const Topbar = ({ children, className, ...props }: ComponentProps<'header'>) => {
   return (
-    <header className={twMerge('h-8 w-screen fixed top-0 bg-red-500', className)} {...props}>
+    <header className={twMerge('h-8 w-screen fixed top-0', className)} {...props}>
       {children}
     </header>
   )
@@ -19,7 +19,7 @@ export const Topbar = ({ children, className, ...props }: ComponentProps<'header
 
 export const Sidebar = ({ children, className, ...props }: ComponentProps<'aside'>) => {
   return (
-    <aside className={twMerge('w-12 overflow-auto bg-blue-500', className)} {...props}>
+    <aside className={twMerge('w-12 overflow-auto', className)} {...props}>
       {children}
     </aside>
   )
@@ -27,7 +27,7 @@ export const Sidebar = ({ children, className, ...props }: ComponentProps<'aside
 
 export const Content = forwardRef<HTMLDivElement, ComponentProps<'div'>>(
   ({ children, className, ...props }, ref) => (
-    <div ref={ref} className={twMerge('flex-1 overflow-auto bg-green-500', className)} {...props}>
+    <div ref={ref} className={twMerge('flex-1 overflow-auto', className)} {...props}>
       {children}
     </div>
   )
