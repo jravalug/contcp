8c3e11de6c3dd81b7b20f39b4ba6769f3f60de33
jravalug <jravalug@gmail.com>
2024-04-04 02:41:47 -0400

    create the router

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,10 +2,36 @@ import './assets/main.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom'
 import App from './App'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/gastos',
+        element: <Link to={`/`}>Gastos</Link>
+      },
+      {
+        path: '/ingresos',
+        element: <Link to={`/`}>Ingresos</Link>
+      },
+      {
+        path: '/tributos',
+        element: <Link to={`/`}>Tributos</Link>
+      },
+      {
+        path: '/reportes',
+        element: <Link to={`/`}>Reportes</Link>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
